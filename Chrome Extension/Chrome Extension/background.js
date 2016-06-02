54d7806ff2ec68c78ce9e9d51b7756b9db4ad891
Eric Warrington <esw5886@rit.edu>
2016-06-01 20:52:22 -0400

    Added opacity slider capability

--- Chrome Extension/Chrome Extension/background.js
+++ Chrome Extension/Chrome Extension/background.js
@@ -9,3 +9,16 @@ function blurImage(image, val)
 	
 	image.setAttribute("style", "filter: blur(" + val + "px); -webkit-filter: blur(" + val + "px)");
 }
+
+function setImageOpacity(image, val)
+{
+	// I want to add a second UI to adjust opacity
+	image.setAttribute("style", "filter: opacity(" + val + "%); -webkit-filter: opacity(" + val + "%)");
+}
+
+/*
+function openSettings()
+{
+	document.getElementById("bg").setAttribute("style", "filter: blur(" + x + "px); -webkit-filter: blur(" + x + "px)");
+}
+*/
